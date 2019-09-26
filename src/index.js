ad3bbb9e64dcec7fdfde15178e335fb73d74ec84
Stadionova <726383@mail.ru>
2019-09-26 21:13:26 +0300

    new changes layout & click cross

--- src/index.js
+++ src/index.js
@@ -1,48 +1,29 @@
-import _ from 'lodash';
 import "./styles.less";
 
-// function component() {
-//   const element = document.createElement('div');
-
-//   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-//   return element;
-// }
-
-// document.body.appendChild(component());
-
-// let modal = document.getElementById("modal");
-// let modalClose = document.getElementById("modal-close");
-// let bookingBtn = document.getElementById("booking-btn");
-
-
+let modal;
+let modalClose;
+let bookingBtn;
 
 function showModal(mouseEvent) {
-  console.log("showModal");
-  let modal = document.getElementById("modal");
   modal.style.display = "block";
 }
 
 function hideModal(mouseEvent) {
-  console.log("hideModal");
-  let modal = document.getElementById("modal");
   modal.style.display = "none";
 }
-
-// setTimeout(showModal, 2000);
-// setTimeout(hideModal, 6000);
 
 function init() {
   console.log("init");
 
-  let modal = document.getElementById("modal");
-  modal.onclick = hideModal;
+  modal = document.getElementById("modal");
+  modalClose = document.getElementById("modal-close");
+  bookingBtn = document.getElementById("booking-btn");
 
-  let bookingBtn = document.getElementById("booking-btn");
+  modalClose.onclick = hideModal;
   bookingBtn.onclick = showModal;
 }
 
-document.addEventListener("DOMContentLoaded", function(event) { 
+document.addEventListener("DOMContentLoaded", function (event) {
   console.log("DOMContentLoaded");
   init();
 });
